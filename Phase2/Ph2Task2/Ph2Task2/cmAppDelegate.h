2dc563eb137357030498c07d6522b2450daffa7d
Epaphos <epaphos.dev@gmail.com>
2013-11-08 16:42:47 +0100

    Initial Commit

---
+++ Phase2/Ph2Task2/Ph2Task2/cmAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  cmAppDelegate.h
+//  Ph2Task2
+//
+//  Created by Cyrill Averbeck on 2013-11-08.
+//  Copyright (c) 2013 Cyrill Averbeck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cmAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Phase2/Ph2Task2/Ph2Task2/cmMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  cmMasterViewController.h
+//  Ph2Task2
+//
+//  Created by Cyrill Averbeck on 2013-11-08.
+//  Copyright (c) 2013 Cyrill Averbeck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class cmDetailViewController;
+
+@interface cmMasterViewController : UITableViewController
+
+@property (strong, nonatomic) cmDetailViewController *detailViewController;
+
+@end
